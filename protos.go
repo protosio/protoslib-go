c7fa4fcaad3ebae2c704a2b36f3472cbac2a2d47
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2017-09-13 17:35:21 +0300

    Initial Protos go library

---
+++ protos.go
@@ -0,0 +1,137 @@
+package protos
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var protosURL string
+
+// DNSResource is a Protos resource for DNS
+type DNSResource struct {
+	Host  string `json:"host"`
+	Value string `json:"value" hash:"-"`
+	Type  string `json:"type"`
+	TTL   int    `json:"ttl" hash:"-"`
+}
+
+// Resource represents a generalised Protos resource
+type Resource struct {
+	Type   string      `json:"type"`
+	Record DNSResource `json:"value"`
+	Status string      `json:"status"`
+	ID     string      `json:"id"`
+}
+
+// Protos client struct
+type Protos struct {
+	URL        string
+	HTTPclient *http.Client
+}
+
+// SetResourceStatus takes a resource ID and sets a new status
+func (p Protos) SetResourceStatus(resourceID string, rstatus string) error {
+
+	statusJSON, err := json.Marshal(&struct {
+		Status string `json:"status"`
+	}{
+		Status: rstatus,
+	})
+	if err != nil {
+		return err
+	}
+
+	url := p.URL + "internal/resource/" + resourceID
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(statusJSON))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := p.HTTPclient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return errors.New(string(b))
+	}
+
+	return nil
+}
+
+// SetStatusBatch takes a list of Resource and applies the same status to all of them
+func (p Protos) SetStatusBatch(resources map[string]Resource, rstatus string) error {
+	for _, resource := range resources {
+		err := p.SetResourceStatus(resource.ID, rstatus)
+		if err != nil {
+			return fmt.Errorf("Could not set status for resource %s: %v", resource.ID, err)
+		}
+	}
+	return nil
+}
+
+// GetResources returns the resources of a specific provider
+func (p Protos) GetResources() (map[string]Resource, error) {
+
+	resourcesReq, err := http.NewRequest("GET", p.URL+"internal/resource/provider", nil)
+	resources := make(map[string]Resource)
+
+	resp, err := p.HTTPclient.Do(resourcesReq)
+	if err != nil {
+		return map[string]Resource{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return map[string]Resource{}, errors.New(string(b))
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&resources)
+	if err != nil {
+		return map[string]Resource{}, err
+	}
+
+	return resources, nil
+}
+
+// RegisterProvider allows an app to register as a provider for a specific resource type
+func (p Protos) RegisterProvider(rtype string) error {
+	payloadJSON, err := json.Marshal(&struct {
+		Type string `json:"type"`
+	}{
+		Type: rtype,
+	})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", p.URL+"internal/provider", bytes.NewBuffer(payloadJSON))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := p.HTTPclient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return errors.New(string(b))
+	}
+
+	return nil
+}
+
+// NewClient returns a client that can be used to interact with Protos
+func NewClient(url string) Protos {
+	return Protos{
+		URL:        url,
+		HTTPclient: &http.Client{},
+	}
+}
