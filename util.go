be5e2813020d3d5e6da66b0a0ce076d9d252c159
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2017-12-05 16:31:15 +0200

    added functionality for working with app IDs

---
+++ util.go
@@ -0,0 +1,21 @@
+package protos
+
+import (
+	"errors"
+	"os"
+)
+
+const (
+	// EnvVarAppID is the name of the environment variable that holds the unique application ID that is used
+	// identifying every request.
+	EnvVarAppID = "APPID"
+)
+
+// GetAppID retrieves the app ID from an evironment variable
+func GetAppID() (string, error) {
+	appID := os.Getenv(EnvVarAppID)
+	if appID == "" {
+		return "", errors.New("APPID environment variable is not set")
+	}
+	return appID, nil
+}
