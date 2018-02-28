550891bc2fdd743931fca96e7c915fd6d5262821
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-02-28 17:49:45 +0200

    added a new method for retrieving a specific resource and split the code in several files

---
+++ consumer.go
@@ -0,0 +1,71 @@
+package protoslib
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	resource "github.com/nustiueudinastea/protos/resource"
+)
+
+// CreateResource creates a Protos resource
+func (p Protos) CreateResource(rsc resource.Resource) (*resource.Resource, error) {
+	rscJSON, err := json.Marshal(rsc)
+	if err != nil {
+		return nil, err
+	}
+
+	url := p.URL + "internal/resource"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(rscJSON))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := p.makeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(res, &rsc)
+	if err != nil {
+		return nil, err
+	}
+	return &rsc, nil
+}
+
+// GetResource retrieves a resources based on the provided ID
+func (p Protos) GetResource(resourceID string) (*resource.Resource, error) {
+	url := p.URL + "internal/resource/" + resourceID
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.makeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	rsc := resource.Resource{}
+	err = json.Unmarshal(res, &rsc)
+	if err != nil {
+		return nil, err
+	}
+	return &rsc, nil
+}
+
+// DeleteResource deletes a resource based on the provided id
+func (p Protos) DeleteResource(resourceID string) error {
+	url := p.URL + "internal/resource/" + resourceID
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = p.makeRequest(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ provider.go
@@ -0,0 +1,124 @@
+package protoslib
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	resource "github.com/nustiueudinastea/protos/resource"
+)
+
+// RegisterProvider allows an app to register as a provider for a specific resource type
+func (p Protos) RegisterProvider(rtype string) error {
+	req, err := http.NewRequest("POST", p.URL+"internal/provider/"+rtype, bytes.NewBuffer([]byte{}))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = p.makeRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeregisterProvider allows an app to register as a provider for a specific resource type
+func (p Protos) DeregisterProvider(rtype string) error {
+	req, err := http.NewRequest("DELETE", p.URL+"internal/provider/"+rtype, bytes.NewBuffer([]byte{}))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = p.makeRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateResourceValue updates a resource's value based on the ID and new value provided
+func (p Protos) UpdateResourceValue(resourceID string, newValue resource.Type) error {
+	payloadJSON, err := json.Marshal(newValue)
+	if err != nil {
+		return err
+	}
+
+	url := p.URL + "internal/resource/" + resourceID
+	req, err := http.NewRequest("UPDATE", url, bytes.NewBuffer(payloadJSON))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = p.makeRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SetResourceStatus takes a resource ID and sets a new status
+func (p Protos) SetResourceStatus(resourceID string, rstatus string) error {
+
+	statusJSON, err := json.Marshal(&struct {
+		Status string `json:"status"`
+	}{
+		Status: rstatus,
+	})
+	if err != nil {
+		return err
+	}
+
+	url := p.URL + "internal/resource/" + resourceID
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(statusJSON))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = p.makeRequest(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SetStatusBatch takes a list of Resource and applies the same status to all of them
+func (p Protos) SetStatusBatch(resources map[string]*resource.Resource, rstatus string) error {
+	for _, resource := range resources {
+		err := p.SetResourceStatus(resource.ID, rstatus)
+		if err != nil {
+			return fmt.Errorf("Could not set status for resource %s: %v", resource.ID, err)
+		}
+	}
+	return nil
+}
+
+// GetResources returns the resources of a specific provider
+func (p Protos) GetResources() (map[string]*resource.Resource, error) {
+
+	resources := Resources{}
+	resourcesReq, err := http.NewRequest("GET", p.URL+"internal/resource/provider", nil)
+	if err != nil {
+		return resources, err
+	}
+
+	payload, err := p.makeRequest(resourcesReq)
+	if err != nil {
+		return resources, err
+	}
+
+	err = json.Unmarshal(payload, &resources)
+	if err != nil {
+		return resources, err
+	}
+
+	return resources, nil
+}
